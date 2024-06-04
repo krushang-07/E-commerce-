c02cfe7a926219e6b1b23130a46ae2ce4eae91c4
krushang-07 <krushangsavaliya45@gmail.com>
2024-06-04 18:51:56 +0530

    navbar homepage basic ui and login and signup ui done

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Home from "./pages/Home";
+//import Home from "./pages/Home";
+// import SignupPage from "./pages/SignupPage";
+// import LoginPage from "./pages/LoginPage";
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
